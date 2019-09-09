087975c6c61460aed3cc7d580072bb0f849b1932
MarshallHo <linkshady428@gmail.com>
2019-09-10 01:34:19 +1000

    first login view

---
+++ Graffiti/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  Graffiti
+//
+//  Created by Teng-Sheng Ho on 2019/9/8.
+//  Copyright © 2019 Mh. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userText: UITextField!
+    @IBOutlet weak var pwText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginAction(_ sender: Any) {
+        
+        if(userText.text != "" && pwText.text != ""){
+            print("Username :"+userText.text!)
+            print("password :"+pwText.text!)
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }else{
+            print("sth wrong")
+        }
+        
+    }
+    
+}
+
