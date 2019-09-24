fb5718b118bc294123197a30230123f4a462f012
MarshallHo <linkshady428@gmail.com>
2019-09-24 14:39:50 +1000

    More views

--- Graffiti/HomeViewController.swift
+++ Graffiti/HomeViewController.swift
@@ -2,99 +2,21 @@
 //  HomeViewController.swift
 //  Graffiti
 //
-//  Created by Teng-Sheng Ho on 2019/9/9.
+//  Created by Teng-Sheng Ho on 2019/9/24.
 //  Copyright © 2019 Mh. All rights reserved.
 //
 
 import UIKit
-import FirebaseDatabase
-import MapKit
-import CoreLocation
 
-class HomeViewController: UIViewController, CLLocationManagerDelegate {
+class HomeViewController: UIViewController {
 
-    @IBOutlet weak var locationText: UITextField!
-    @IBOutlet weak var uploadButton: UIButton!
-    @IBOutlet weak var chooseButton: UIButton!
-    @IBOutlet weak var imageView: UIImageView!
-    
-    var pickImage = UIImagePickerController()
-    var ref: DatabaseReference! = Database.database().reference()
-    let locationManager = CLLocationManager()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Ask for Authorisation from the User.
-        self.locationManager.requestAlwaysAuthorization()
-        
-        // For use in foreground
-        self.locationManager.requestWhenInUseAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-        }
-        
+
+        // Do any additional setup after loading the view.
     }
     
-    
-    @IBAction func uploadButton(_ sender: Any) {
-    }
-    @IBAction func chooseAction(_ sender: Any) {
-        
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        
-        alert.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
-            self.openCamera()
-        }))
-        
-        alert.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { _ in
-            self.openGallary()
-        }))
-        
-        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
-        
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    //MARK: - Location manager
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-        locationText.text = "locations = \(locValue.latitude) \(locValue.longitude)"
-    }
-    
-    
-    //MARK: - Open the camera
-    func openCamera(){
-        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera)){
-            pickImage.sourceType = UIImagePickerController.SourceType.camera
-            //If you dont want to edit the photo then you can set allowsEditing to false
-            pickImage.allowsEditing = true
-            pickImage.delegate = self
-            self.present(pickImage, animated: true, completion: nil)
-        }
-        else{
-            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
-    
-    //MARK: - Choose image from gallery
-    func openGallary(){
-        pickImage.sourceType = UIImagePickerController.SourceType.photoLibrary
-        //If you dont want to edit the photo then you can set allowsEditing to false
-        pickImage.allowsEditing = true
-        pickImage.delegate = self
-        self.present(pickImage, animated: true, completion: nil)
-    }
-    
-    /*
-    @IBAction func uploadAction(_ sender: Any) {
-        ref.child("image").child("2123").setValue(["username":Text.text!])
-    }
-    */
+
     /*
     // MARK: - Navigation
 
@@ -105,23 +27,4 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate {
     }
     */
 
-}
-
-//MARK: - UIImagePickerControllerDelegate
-extension HomeViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-    
-    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
-            self.imageView.image = editedImage
-        }
-        
-        //Dismiss the UIImagePicker after selection
-        picker.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.isNavigationBarHidden = false
-        self.dismiss(animated: true, completion: nil)
-    }
 }
---
+++ Graffiti/PhotoViewController.swift
@@ -0,0 +1,166 @@
+//
+//  PhotoViewController.swift
+//  Graffiti
+//
+//  Created by Teng-Sheng Ho on 2019/9/9.
+//  Copyright © 2019 Mh. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import MapKit
+import CoreLocation
+import Firebase
+
+class PhotoViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var locationText: UITextField!
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var pickImage = UIImagePickerController()
+    var ref: DatabaseReference! = Database.database().reference()
+    let locationManager = CLLocationManager()
+    var imageID:String!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    
+    @IBAction func uploadButton(_ sender: Any) {
+    }
+    @IBAction func chooseAction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { _ in
+            self.openGallary()
+        }))
+        
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Location manager
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        locationText.text = "\(locValue.latitude) \(locValue.longitude)"
+    }
+    
+    
+    //MARK: - Open the camera
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera)){
+            pickImage.sourceType = UIImagePickerController.SourceType.camera
+            //If you dont want to edit the photo then you can set allowsEditing to false
+            pickImage.allowsEditing = true
+            pickImage.delegate = seßlf
+            self.present(pickImage, animated: true, completion: nil)
+        }
+        else{
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    //MARK: - Choose image from gallery
+    func openGallary(){
+        pickImage.sourceType = UIImagePickerController.SourceType.photoLibrary
+        //If you dont want to edit the photo then you can set allowsEditing to false
+        pickImage.allowsEditing = true
+        pickImage.delegate = self
+        self.present(pickImage, animated: true, completion: nil)
+    }
+    
+    func uploadMedia(completion: @escaping (_ url: String?) -> Void) {
+        
+        let path = imageID + ".jpg"
+        let storageRef = Storage.storage().reference().child("images").child(path)
+
+        if let uploadData = imageView.image?.jpegData(compressionQuality: 0.1) {
+            storageRef.putData(uploadData, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    print("error")
+                    completion(nil)
+                } else {
+                    storageRef.downloadURL(completion: { (url, error) in
+                        print(url?.absoluteString as Any)
+                        completion(url?.absoluteString)
+                    })
+                    
+                    //  completion((metadata?.downloadURL()?.absoluteString)!))
+                    // your uploaded photo url.
+                    
+                    
+                }
+            }
+        }
+    }
+    
+    @IBAction func uploadAction(_ sender: Any) {
+        
+        imageID = (self.ref?.child("Image").childByAutoId().key)
+        uploadMedia() { url in
+            guard url != nil else { return }
+            self.ref?.child("images").childByAutoId().setValue([
+                "uid"              : Auth.auth().currentUser?.uid,
+                "description"     : "lllooo",
+                "location"       : self.locationText.text!,
+                ])
+        
+        
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+//MARK: - UIImagePickerControllerDelegate
+
+extension PhotoViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            self.imageView.image = editedImage
+        }
+        
+        //Dismiss the UIImagePicker after selection
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.isNavigationBarHidden = false
+        self.dismiss(animated: true, completion: nil)
+    }
+}
